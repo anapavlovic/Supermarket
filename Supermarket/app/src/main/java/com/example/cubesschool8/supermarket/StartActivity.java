819b0df77e6abda5527235401aac4a4c26780f16
Ana Pavlovic <agafana@gmail.com>
2016-09-07 09:53:02 +0200

    mk

---
+++ Supermarket/app/src/main/java/com/example/cubesschool8/supermarket/StartActivity.java
@@ -0,0 +1,15 @@
+package com.example.cubesschool8.supermarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cubesschool8 on 9/7/16.
+ */
+public class StartActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+    }
+}
