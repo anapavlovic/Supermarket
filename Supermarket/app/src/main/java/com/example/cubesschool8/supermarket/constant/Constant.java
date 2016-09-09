1a43d261c7d85d9a5608868140edd512c1a6914b
Ana Pavlovic <agafana@gmail.com>
2016-09-09 11:49:20 +0200

    api requests

---
+++ Supermarket/app/src/main/java/com/example/cubesschool8/supermarket/constant/Constant.java
@@ -0,0 +1,16 @@
+package com.example.cubesschool8.supermarket.constant;
+
+/**
+ * Created by cubesschool8 on 9/9/16.
+ */
+public class Constant {
+    private static String BASE_URL = "http://shop.cubes.rs/";
+
+    public static String GET_TOKEN_URL = BASE_URL + "phone-home-gettoken";
+    public static String LOGIN_URL = BASE_URL + "phone-user";
+    public static String SIGNUP_URL = BASE_URL + "phone-signup";
+    public static String FORGOT_PASSWORD_URL = BASE_URL + "phone-user-forgotpassword";
+    public static String CITY_URL = BASE_URL + "phone-helper-places";
+    public static String RESERVATION_URL = BASE_URL + "phone-helper-reservation";
+    public static String CATEGORY_URL = BASE_URL + "phone-categories";
+}
---
+++ Supermarket/app/src/main/java/com/example/cubesschool8/supermarket/data/DataContainer.java
@@ -0,0 +1,9 @@
+package com.example.cubesschool8.supermarket.data;
+
+/**
+ * Created by cubesschool8 on 9/9/16.
+ */
+public class DataContainer {
+
+    public static String TOKEN;
+}
